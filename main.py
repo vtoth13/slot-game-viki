0fd59fad07aad784fd2dbdab510d98180cffa5da
vtoth13 <snapviki13@gmail.com>
2024-02-28 20:39:05 +0100

    Add definition for lines

--- main.py
+++ main.py
@@ -82,4 +82,20 @@ def deposit():
         else:
             print("Please enter a number.")
 
-    return amount        
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(
+            f"Enter the number of lines to bet on (1-{MAX_LINES})? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a valid number.")
+        else:
+            print("Please enter a number.")
+
+    return lines
