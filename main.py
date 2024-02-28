ab7cc2817ba42948d2760fbcd3dfdabac2e2c7b5
vtoth13 <snapviki13@gmail.com>
2024-02-28 20:47:00 +0100

    Add definition for bet

--- main.py
+++ main.py
@@ -99,3 +99,19 @@ def get_number_of_lines():
             print("Please enter a number.")
 
     return lines
+
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
